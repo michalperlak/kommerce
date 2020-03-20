46dfbb7b3da8279ef32944d32c98ef2454f51c97
Michał Perlak <michalperlak@windowslive.com>
2020-03-20 21:16:50 +0100

    JUnit extension for api server

---
+++ kommerce-web-api/src/test/kotlin/com/github/kommerce/web/ServerExtension.kt
@@ -0,0 +1,25 @@
+package com.github.kommerce.web
+
+import org.junit.jupiter.api.extension.AfterAllCallback
+import org.junit.jupiter.api.extension.BeforeAllCallback
+import org.junit.jupiter.api.extension.ExtensionContext
+
+internal class ServerExtension : BeforeAllCallback, AfterAllCallback {
+    private val server: ApiServer = TODO()
+
+    override fun beforeAll(context: ExtensionContext) {
+        val serverThread = Thread {
+            server
+                .start()
+                .onDispose()
+                .block()
+        }
+        serverThread.start()
+    }
+
+    override fun afterAll(context: ExtensionContext) = server.stop()
+
+    companion object {
+        const val TEST_PORT = 9876
+    }
+}
